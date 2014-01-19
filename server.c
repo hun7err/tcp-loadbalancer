2899465bb97fd04a7c9f256d933cd0a7f953b949
hun7err <hun7err@gmail.com>
2014-01-19 22:41:52 +0100

    Proxy functionality works for 1 client and 1 node, yay!

--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/ioctl.h> // temp
@@ -6,6 +7,81 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
+#include <pthread.h>
+
+#define ISENGARD nodes[0]
+#define MORDOR nodes[1]
+#define KHAZADDUM nodes[2]
+
+struct node_info {
+    
+};
+
+struct out_data {
+    int client_sd;
+    int* out_sd;
+    int out_port;
+};
+
+struct client_data {
+    int client_sd;
+    int* out_sd;
+};
+
+const char* servers[] = {"192.168.122.70", "192.168.122.46", "192.168.122.32"};
+
+void *clientInHandler(void* client_data_ptr)
+{
+    struct client_data data = *(struct client_data*)client_data_ptr;
+
+    char buffer[1024];
+    while(*(data.out_sd) < 0)
+    {
+        if(*data.out_sd == -2)
+        {
+            return NULL;
+        }
+    }
+
+    int n;
+    while((n = recv(data.client_sd, buffer, 1024, 0)) != -1)
+    {
+        send(*(data.out_sd), buffer, n, 0);
+    }
+    close(data.client_sd);
+
+    return NULL;
+}
+
+void *clientOutHandler(void* out_data_ptr)
+{
+    struct out_data data = *(struct out_data*)out_data_ptr;
+    int sd = socket(AF_INET, SOCK_STREAM, 0);
+    char buffer[1024];
+
+    struct sockaddr_in addr;
+    addr.sin_port = htons(data.out_port);
+    inet_pton(AF_INET, servers[0], &(addr.sin_addr));
+    addr.sin_family = AF_INET;
+
+    int rc = connect(sd, (struct sockaddr*)&addr, sizeof(addr));
+    if(rc < 0)
+    {
+        *(data.out_sd) = -2;
+        perror("Could not connect()\n");
+        return NULL;
+    }
+    *(data.out_sd) = sd;
+
+    int len;
+    while((len = recv(*(data.out_sd), buffer, 1024, 0)) != -1)
+    {
+        send(data.client_sd, buffer, len, 0);
+    }
+    close(sd);
+
+    return NULL;
+}
 
 int main(int argc, char ** argv)
 {
@@ -51,7 +127,11 @@ int main(int argc, char ** argv)
     server_addr.sin_addr.s_addr = ((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr.s_addr; // .s_addr ?
     server_addr.sin_port = htons(in_port);
 
-    printf("Binding socket to %s:%d\n", inet_ntoa(((struct sockaddr_in*)&ifr.ifr_addr)->sin_addr), in_port);
+    char address[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &((struct sockaddr_in*)&ifr.ifr_addr)->sin_addr, address, INET_ADDRSTRLEN);
+
+    printf("Binding socket to %s:%d\n", address, in_port);
+    int out_sd = -1;
 
     if(bind(sd_in, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
     {
@@ -73,7 +153,10 @@ int main(int argc, char ** argv)
     struct sockaddr_in client_addr;
     int client_addr_len = sizeof(client_addr);
 
-    do {
+    pthread_t in_handler, out_handler;
+    int rc1, rc2;
+
+    while(1) {
         memset(&client_addr, 0, sizeof(client_addr));
         client_sd = accept(sd_in, (struct sockaddr*)&client_addr, &client_addr_len);
         if(client_sd == -1)
@@ -82,11 +165,24 @@ int main(int argc, char ** argv)
             perror("Error handling client connection");
             return -1;
         }
-        printf("Incoming connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        close(client_sd);
-        return 0;
+        inet_ntop(AF_INET, &(client_addr.sin_addr), address, INET_ADDRSTRLEN);
+        printf("Incoming connection from %s:%d\n", address, ntohs(client_addr.sin_port));
+        struct out_data* data = malloc(sizeof(struct out_data));
+        data->client_sd = client_sd;
+        data->out_port = out_port;
+        data->out_sd = &out_sd;
 
-    } while(0);
+        struct client_data* client_data_ptr = malloc(sizeof(struct client_data));
+        client_data_ptr->client_sd = client_sd;
+        client_data_ptr->out_sd = &out_sd;
+
+        rc1 = pthread_create(&in_handler, NULL, clientInHandler, client_data_ptr);
+        rc2 = pthread_create(&out_handler, NULL, clientOutHandler, data);
+
+        //close(client_sd);
+        //return 0;
+
+    };
     
     close(sd_in);
 
