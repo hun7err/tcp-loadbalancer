4c214b2e439877bce7e20d4950afc28837029eb2
hun7err <hun7err@gmail.com>
2014-02-03 19:39:47 +0100

    Git, plz. Stahp.

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-gcc loadbalancer.c sockets.c hex.c commands.c fdescriptors.c -o loadbalancer -Wall
-# -Werror
+gcc src/loadbalancer.c src/nodes.c src/sockets.c src/commands.c src/fdescriptors.c -o loadbalancer -Wall -Werror
+
