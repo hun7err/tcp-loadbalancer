7bea527e8e3bfbfdbfe971730454e0012a218265
hun7err <hun7err@gmail.com>
2014-02-04 07:45:06 +0100

    Fixed make.sh

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
-gcc src/loadbalancer.c src/nodes.c src/sockets.c src/commands.c src/fdescriptors.c -o loadbalancer -Wall -Werror
+gcc src/loadbalancer.c src/nodes.c src/sockets.c src/commands.c src/packet.c src/fdescriptors.c -o loadbalancer -Wall -Werror
 
